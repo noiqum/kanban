ed3ae02ea8a97aa126f9e110bd58dd0046479f15
noiqum <onurcoskun99@gmail.com>
2024-04-05 21:52:06 +0300

    update layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={pjs.className}>
-      <body>
+      <body className="pb-24 h-screen overflow-hidden">
         <Providers>
           <Navbar />
           {children}
