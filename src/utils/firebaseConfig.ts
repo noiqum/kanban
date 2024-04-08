8e8782a0772b9688b4f03afe0f5f68b7caa667c5
noiqum <onurcoskun99@gmail.com>
2024-04-09 00:03:02 +0300

    firebase config

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Paste your Firebase config here
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: "kanban-419120",
+  storageBucket: "kanban-419120.appspot.com",
+  messagingSenderId: "697428607616",
+  appId: "1:697428607616:web:b79c55cff0378075591c65",
+  measurementId: "G-05G4GJY0F1",
 };
 
 const app = initializeApp(firebaseConfig);
